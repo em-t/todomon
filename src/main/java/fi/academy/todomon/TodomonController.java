56aa915f200c88d8d74abbf15761b88a18b90b1c
kiertorata <eva.taivalsaari@gmail.com>
2018-10-30 16:01:03 +0200

    fronttikokeiluja

--- src/main/java/fi/academy/todomon/TodomonController.java
+++ src/main/java/fi/academy/todomon/TodomonController.java
@@ -3,6 +3,7 @@ package fi.academy.todomon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +12,17 @@ public class TodomonController {
 
     @Autowired
     private UsersRepository repo;
+
+
+    // ============ emmun testausmetodit ============
+    @GetMapping("/main")
+    public String etusivuTestaus() {
+        return "main";
+    }
+
+    @GetMapping("/add")
+    public String lisaysTestaus() {
+        return "add";
+    }
 }
 
